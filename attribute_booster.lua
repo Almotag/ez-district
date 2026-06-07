6b98758cc18276733160f5020b96cf53be55e2e2
Almo <contactalmo.pro@gmail.com>
2026-06-07 19:19:37 +0200

    Add attribute booster for player speed and repair

---
+++ attribute_booster.lua
@@ -0,0 +1,10 @@
+local lplayer = game.Players.LocalPlayer.Name
+
+workspace[lplayer]:SetAttribute("speedboost",speedboost or 1)
+workspace[lplayer]:SetAttribute("vaultspeed",vaultspeed or 1)
+workspace[lplayer]:SetAttribute("repairboost",repairboost or 1)
+workspace[lplayer]:SetAttribute("skillcheckspeed",skillcheckspeed or 1)
+--workspace[lplayer]:SetAttribute("healboost",healboost or 1)
+--workspace[lplayer]:SetAttribute("healnerf",healnerf or 1)
+--workspace[lplayer]:SetAttribute("skillcheckfrequency",skillcheckfrequency or 1)
+--workspace[lplayer]:SetAttribute("unhookspeed",unhookspeed or 1)
