f429c36ce9526c604e97d0c5d5fba2fb948675bd
Almo <contactalmo.pro@gmail.com>
2026-06-10 20:00:58 +0200

    Update attribute_booster.lua

--- attribute_booster.lua
+++ attribute_booster.lua
@@ -1,9 +1,9 @@
 local lplayer = game.Players.LocalPlayer.Name
 
-workspace[lplayer]:SetAttribute("speedboost",speedboost or 1)
-workspace[lplayer]:SetAttribute("vaultspeed",vaultspeed or 1)
-workspace[lplayer]:SetAttribute("repairboost",repairboost or 1)
-workspace[lplayer]:SetAttribute("skillcheckspeed",skillcheckspeed or 1)
+workspace[lplayer]:SetAttribute("speedboost",_G.speedboost or 1)
+workspace[lplayer]:SetAttribute("vaultspeed",_G.vaultspeed or 1)
+workspace[lplayer]:SetAttribute("repairboost",_G.repairboost or 1)
+workspace[lplayer]:SetAttribute("skillcheckspeed",_G.skillcheckspeed  or 1)
 --workspace[lplayer]:SetAttribute("healboost",healboost or 1)
 --workspace[lplayer]:SetAttribute("healnerf",healnerf or 1)
 --workspace[lplayer]:SetAttribute("skillcheckfrequency",skillcheckfrequency or 1)
