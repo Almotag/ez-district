ed279ef99d56a306ec02871117cb95598290697e
Almo <contactalmo.pro@gmail.com>
2026-06-10 20:52:47 +0200

    Update and rename players-esp.lua to surv-esp.lua

---
+++ surv-esp.lua
@@ -0,0 +1,23 @@
+if _G.survivors_esp == nil then
+	_G.survivors_esp = false
+while _G.survivors_esp do
+		for _, player in pairs(game.Players:GetPlayers()) do
+			if player ~= game.Players.LocalPlayer and workspace:FindFirstChild(player.Name) then
+				if player.Team.Name == "Survivors" then
+					if not workspace[player.Name]:FindFirstChild("SurvivorHighlight") then
+						h = Instance.new("Highlight")
+						h.Name = "SurvivorHighlight"
+						h.FillColor = player.TeamColor.Color
+						h.OutlineColor = player.TeamColor.Color
+						h.Parent = workspace[player.Name]
+					end
+				else
+					if workspace[player.Name]:FindFirstChild("SurvivorHighlight") then
+						workspace[player.Name].SurvivorHighlight:Destroy()
+					end
+				end
+			end
+		end
+		task.wait(1)
+	end
+end)
